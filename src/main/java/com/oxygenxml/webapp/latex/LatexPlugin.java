e35e7b325a379f9e143cdc36a20d4f5f90a20c59
Mihaela Calotescu <mihaela@oxygenxml.com>
2023-03-08 17:28:18 +0200

    WA-6320 Added plugin class and id.

---
+++ src/main/java/com/oxygenxml/webapp/latex/LatexPlugin.java
@@ -0,0 +1,18 @@
+package com.oxygenxml.webapp.latex;
+
+import ro.sync.exml.plugin.Plugin;
+import ro.sync.exml.plugin.PluginDescriptor;
+
+/**
+ * Plug-in used to render and edit Latex equations.
+ */
+public class LatexPlugin extends Plugin {
+  /**
+   * Constructor.
+   * 
+   * @param descriptor Plugin descriptor.
+   */
+  public LatexPlugin(PluginDescriptor descriptor) {
+    super(descriptor);
+  }
+}
